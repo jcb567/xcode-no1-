eed4fd3ba753d30518dea69f7a04fedbe57843af
Jake Cameron Booth <jakecameronbooth@Jakes-MacBook-Pro.local>
2017-03-10 13:30:53 +0000

    We can now add numbers

--- Swift Project/ViewController.swift
+++ Swift Project/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        helloLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
